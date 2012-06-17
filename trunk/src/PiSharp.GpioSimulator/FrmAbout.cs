3a8d6d9db66d08942cafd7c51a4c534b9df86e3e
andycb <andycb@users.noreply.github.com>
2012-06-17 21:28:40 +0000

    Initial code check-in
    git-svn-id: https://pisharp.svn.codeplex.com/svn@17173 ddf9c79f-3887-47c2-86f8-040505c24a32

---
+++ trunk/src/PiSharp.GpioSimulator/FrmAbout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace PiSharp.GpioSimulator
+{
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void Credit_LinkClicked(object sender, MouseEventArgs e)
+        {
+            Process.Start(((LinkLabel)sender).Tag.ToString());
+        }
+
+        private void CmdOkay_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+    }
+}
