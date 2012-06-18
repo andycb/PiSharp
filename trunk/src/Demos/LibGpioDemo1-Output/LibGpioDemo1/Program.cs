6c2c2503b962ec7ac0624da65315d9cd22bc79ec
andycb <andycb@users.noreply.github.com>
2012-06-18 12:37:46 +0000

    Add demo code
    git-svn-id: https://pisharp.svn.codeplex.com/svn@17198 ddf9c79f-3887-47c2-86f8-040505c24a32

---
+++ trunk/src/Demos/LibGpioDemo1-Output/LibGpioDemo1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace LibGpioDemo1
+{
+    using System.Threading;
+
+    // Pi# Namespaces
+    using PiSharp.LibGpio;
+    using PiSharp.LibGpio.Entities;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use test mode
+            LibGpio.Gpio.TestMode = true;
+
+            // Setup all pins for output (using Broadcom numbers)
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.Four, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.Seventeen, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.Eighteen, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.TwentyOne, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.TwentyTwo, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.TwentyThree, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.TwentyFour, Direction.Output);
+            LibGpio.Gpio.SetupChannel(BroadcomPinNumber.TwentyFive, Direction.Output);
+
+            // Loop around doing an LED chase
+            while (true)
+            {
+                // Turn on
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Four, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Seventeen, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Eighteen, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyOne, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyTwo, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyThree, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyFour, true);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyFive, true);
+                Thread.Sleep(200);
+
+                // Turn off
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Four, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Seventeen, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.Eighteen, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyOne, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyTwo, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyThree, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyFour, false);
+                Thread.Sleep(200);
+                LibGpio.Gpio.OutputValue(BroadcomPinNumber.TwentyFive, false);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ trunk/src/Demos/LibGpioDemo2-Input/LibGpioDemo2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LibGpioDemo2
+{
+    using System.Threading;
+
+    // Pi# Namespaces
+    using PiSharp.LibGpio;
+    using PiSharp.LibGpio.Entities;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use test mode
+            LibGpio.Gpio.TestMode = true;
+
+            // Setup one pin for input and one for output (using Raspberry Pi numbers)
+            LibGpio.Gpio.SetupChannel(RaspberryPinNumber.Seven, Direction.Input);
+            LibGpio.Gpio.SetupChannel(RaspberryPinNumber.Zero, Direction.Output);
+
+            // Loop around and set the value of GPIO 0 to the inverse of GPIO 7
+            // ie. If the button is pressed, the LED will go out
+            var oldInput = true;
+            while (true)
+            {
+                var newInput = LibGpio.Gpio.ReadValue(RaspberryPinNumber.Seven);
+                if (newInput != oldInput)
+                {
+                    LibGpio.Gpio.OutputValue(BroadcomPinNumber.Seventeen, !newInput);
+                    oldInput = newInput;
+                }
+            }
+        }
+    }
+}
